604ffb7a5a475454ddacae344026e2ee865bf824
Fayazuddin Mohammed <mohammed.161@wright.edu>
2022-04-20 16:38:36 -0400

    Updating Doxygen comments in config.h file for addressing issue #71
    Updating Doxygen comments in config.h file for addressing issue #71

--- src/config.h
+++ src/config.h
@@ -48,6 +48,8 @@
 #define CRAFT_KEY_CHAT 't'
 #define CRAFT_KEY_COMMAND '/'
 #define CRAFT_KEY_SIGN '`'
+
+/**Creating key binding for [issue] github.com/WSU-CEG-6110-4410/ADAD-Craft/issues/71 */   
 #define CRAFT_KEY_CLOSE_TERMINAL 'M'
 
 /// Addresses issue #22 at link https://github.com/orgs/WSU-CEG-6110-4410/projects/9/views/1
